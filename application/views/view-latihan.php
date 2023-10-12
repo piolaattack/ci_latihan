cf831a162ab293fb14a3338f7457026be77dbdd3
piolaattack <piolaattack@gmail.com>
2023-10-12 16:36:02 +0700

    first commit

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+</body>
+    hallo kawan.. yuk kita belajar web programing..!!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " .$hasil; ?>
+
+</body>
+
+</html>
